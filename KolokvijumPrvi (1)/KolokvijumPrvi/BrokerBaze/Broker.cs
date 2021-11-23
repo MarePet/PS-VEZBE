be6d50808c7c367f5e4c65ef4e886ec487991d97
MarePet <74553764+MarePet@users.noreply.github.com>
2021-11-23 11:16:32 +0100

    Add files via upload

---
+++ KolokvijumPrvi (1)/KolokvijumPrvi/BrokerBaze/Broker.cs
@@ -0,0 +1,85 @@
+﻿using Domen;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrokerBaze
+{
+    public class Broker
+    {
+        private SqlConnection connection;
+
+        public Broker()
+        {
+            connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Kolokvijum;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public void OpenConnection()
+        {
+            connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+
+        public List<Predmet> VratiSvePredmete()
+        {
+            List<Predmet> predmeti = new List<Predmet>();
+            SqlCommand sqlCommand = connection.CreateCommand();
+            sqlCommand.CommandText = "select * from predmet";
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Predmet p = new Predmet { 
+                    PredmetId = reader.GetInt32(0),
+                    Naziv = reader.GetString(1),
+                    ESPB = reader.GetInt32(2)
+                };
+                predmeti.Add(p);
+            }
+            return predmeti;
+        }
+
+        public List<Prijava> VratiSvePrijave()
+        {
+            List<Prijava> prijave = new List<Prijava>();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "select * from prijava pr join Predmet p on(pr.PredmetId=p.PredmetId)";
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Prijava p = new Prijava
+                {
+                    PrijavaId = (int)reader[0],
+                    Ime = (string)reader[1],
+                    Prezime = (string)reader[2],
+                    Ocena = (int)reader[3],
+                    Predmet = new Predmet
+                    {
+                        PredmetId = (int)reader[4],
+                        Naziv = (string)reader[6],
+                        ESPB = (int) reader[7]
+                    }
+                };
+                prijave.Add(p);
+            }
+            return prijave;
+        }
+
+        public void SacuvajPrijavu(Prijava p)
+        {
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "insert into prijava values (@ime, @prezime, @ocena, @predmet)";
+            command.Parameters.AddWithValue("@ime", p.Ime);
+            command.Parameters.AddWithValue("@prezime", p.Prezime);
+            command.Parameters.AddWithValue("@ocena", p.Ocena);
+            command.Parameters.AddWithValue("@predmet", p.Predmet.PredmetId);
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ PrimerKolokvijuma/PrimerKolokvijuma/AppLogic/Controller.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseBroker;
+using Domain;
+
+namespace AppLogic
+{
+    public class Controller
+    {
+        private static Controller instance;
+        private static Broker broker = new Broker();
+        private Controller(){
+        }
+        public static Controller Instance {
+            get {
+                if (instance == null)
+                { 
+                    instance = new Controller(); 
+                }
+                return instance;
+            }
+        }
+
+        public List<Projekat> GetProjektiRukovodilac(Korisnik korisnik)
+        {
+            List<Projekat> projekats = new List<Projekat>();
+            try
+            {
+               broker.OpenConnection();
+               projekats=broker.GetProjektiBazaRukovodilac(korisnik);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                broker.CloseConnection();
+            }
+            return projekats;
+         }
+
+        public List<Projekat> GetProjektiUcesnik(Korisnik korisnik)
+        {
+            List<Projekat> projekats = new List<Projekat>();
+            try
+            {
+                broker.OpenConnection();
+                projekats = broker.GetProjektiBazaUcesnik(korisnik);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                broker.CloseConnection();
+            }
+            return projekats;
+        }
+
+        public List<Korisnik> GetUcesnik(List<Projekat> projekatsRukovodilac, Korisnik k)
+        {
+            List<Korisnik> korisniks = new List<Korisnik>();
+            try
+            {
+                broker.OpenConnection();
+               korisniks=broker.GetUcesnikBaza(projekatsRukovodilac,k);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                broker.CloseConnection();
+            }
+            return korisniks;
+        }
+
+        public void ObrisiUcesnika(Korisnik korisnikZaBrisanje, Projekat projekatZaBisanje)
+        {
+            try
+            {
+                broker.OpenConnection();
+                broker.ObrisiUcesnikaBaza(korisnikZaBrisanje,projekatZaBisanje);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                broker.CloseConnection();
+            }
+        }
+
+        public void AddUcesnik(Korisnik korisnik, Projekat projekat)
+        {
+            try
+            {
+                broker.OpenConnection();
+                broker.AddUcesnikBaza(korisnik, projekat);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                broker.CloseConnection();
+            }
+        }
+
+        public List<Korisnik> GetAllFreeUcesnici(Projekat selectedItem, Korisnik korisnik)
+        {
+            List<Korisnik> korisniks = new List<Korisnik>();
+            try
+            {
+                broker.OpenConnection();
+                korisniks = broker.GetAllFreeUcesniciBaza(selectedItem, korisnik);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                broker.CloseConnection();
+            }
+            return korisniks;
+        }
+
+        public List<Korisnik> GetUcesnik1(Projekat selectedItem, Korisnik korisnik)
+        {
+            List<Korisnik> korisniks = new List<Korisnik>();
+            try
+            {
+                broker.OpenConnection();
+                korisniks = broker.GetUcesnikBaza1(selectedItem, korisnik);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                broker.CloseConnection();
+            }
+            return korisniks;
+        }
+
+        public Korisnik Login(Korisnik korisnik) {
+
+            try
+            {
+                broker.OpenConnection();
+                List<Korisnik> korisniks = broker.GetUsers();
+                foreach (Korisnik k in korisniks) {
+                    if (k.Username == korisnik.Username &&
+                            k.Password == korisnik.Password) {
+                        return k;
+                    }
+                }return null;
+            }
+            finally
+            {
+                broker.CloseConnection();
+            }
+        }
+    }
+}
